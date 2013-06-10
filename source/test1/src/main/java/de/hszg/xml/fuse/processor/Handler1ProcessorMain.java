370e4bb7aefa27b421cdb4ff0e161836bd17195d
Mario Reichel <mario@mario-Pavilion.(none)>
2013-06-10 15:07:22 +0200

    schon mal ein Handler Prozessor

---
+++ source/test1/src/main/java/de/hszg/xml/fuse/processor/Handler1ProcessorMain.java
@@ -0,0 +1,15 @@
+package de.hszg.xml.fuse.processor;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+import com.sun.org.apache.xerces.internal.dom.DeferredElementNSImpl;
+
+public abstract class Handler1ProcessorMain implements Processor {
+	
+	protected DeferredElementNSImpl mainConvert(DeferredElementNSImpl oldXML) {
+		
+		return null;
+	}
+
+}
---
+++ source/test1/src/main/java/de/hszg/xml/fuse/processor/Handler1ProcessorPC.java
@@ -0,0 +1,16 @@
+package de.hszg.xml.fuse.processor;
+
+import org.apache.camel.Exchange;
+
+import com.sun.org.apache.xerces.internal.dom.DeferredElementNSImpl;
+
+public class Handler1ProcessorPC extends Handler1ProcessorMain {
+
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		exchange.setOut(exchange.getIn());
+		DeferredElementNSImpl body = (DeferredElementNSImpl) exchange.getIn().getBody();
+		System.out.println("PC: " + body.getTextContent());
+	}
+
+}
