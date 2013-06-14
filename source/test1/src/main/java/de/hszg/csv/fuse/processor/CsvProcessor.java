163d4533a35278b628c7aa3c6e4881bd344e5304
Patrick <p.breite.neu@web.de>
2013-06-14 13:40:11 +0200

    csvParsing

--- source/test1/src/main/java/de/hszg/csv/fuse/processor/CsvProcessor.java
+++ source/test1/src/main/java/de/hszg/csv/fuse/processor/CsvProcessor.java
@@ -1,24 +1,15 @@
 package de.hszg.csv.fuse.processor;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
-import au.com.bytecode.opencsv.CSVReader;
-
 public class CsvProcessor implements Processor {
+	private String xmlFileName = "src/test/resources/testData/handler2/searchResponce1.xml";
+	CsvToXmlParser parser = new CsvToXmlParser();
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		try (InputStream input = getClass().getResourceAsStream(
-				"testData/handler2/searchRespone1.csv");
-				BufferedReader bReader = new BufferedReader(
-						new InputStreamReader(input));
-				CSVReader reader = new CSVReader(bReader)) {
-			
-		}
-
+		String exchangeBody = exchange.getIn().getBody().toString();
+		
+		parser.convertFile(exchangeBody, xmlFileName, ",");
 	}
 }
--- source/test1/src/main/java/de/hszg/csv/fuse/processor/CsvToXmlParser.java
+++ source/test1/src/main/java/de/hszg/csv/fuse/processor/CsvToXmlParser.java
@@ -2,9 +2,9 @@ package de.hszg.csv.fuse.processor;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,111 +23,110 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-
 import au.com.bytecode.opencsv.CSVReader;
 
 public class CsvToXmlParser {
-	 // Protected Properties
-	
-    protected DocumentBuilderFactory domFactory = null;
-    protected DocumentBuilder domBuilder = null;
-    public static void main(String[] args) {
-    	CsvToXmlParser parser = new CsvToXmlParser();
-		parser.convertFile("src/test/resources/testData/handler2/searchResponce1.csv", "src/test/resources/testData/handler2/searchResponce1.xml", ",");
+	// Protected Properties
+
+	protected DocumentBuilderFactory domFactory = null;
+	protected DocumentBuilder domBuilder = null;
+
+	public CsvToXmlParser() {
+		try {
+			domFactory = DocumentBuilderFactory.newInstance();
+			domBuilder = domFactory.newDocumentBuilder();
+		} catch (FactoryConfigurationError exp) {
+			System.err.println(exp.toString());
+		} catch (ParserConfigurationException exp) {
+			System.err.println(exp.toString());
+		} catch (Exception exp) {
+			System.err.println(exp.toString());
+		}
+
 	}
-    public CsvToXmlParser() {
-        try {
-            domFactory = DocumentBuilderFactory.newInstance();
-            domBuilder = domFactory.newDocumentBuilder();
-        } catch (FactoryConfigurationError exp) {
-            System.err.println(exp.toString());
-        } catch (ParserConfigurationException exp) {
-            System.err.println(exp.toString());
-        } catch (Exception exp) {
-            System.err.println(exp.toString());
-        }
 
-    }
+	public void convertFile(String csvString, String xmlFileName,
+			String delimiter) {
 
-    public void convertFile(String csvFileName, String xmlFileName,
-                    String delimiter) {
+		BufferedReader csvReader;
+		try {
+			Document newDoc = domBuilder.newDocument();
+			// Root element
+			Element rootElement = newDoc.createElement("CsvHandler");
+			newDoc.appendChild(rootElement);
+			// Read csv file
+			csvReader = new BufferedReader(new StringReader(csvString));
 
-        BufferedReader csvReader;
-        try {
-            Document newDoc = domBuilder.newDocument();
-            // Root element
-            Element rootElement = newDoc.createElement("CsvHandler");
-            newDoc.appendChild(rootElement);
-            // Read csv file
-            csvReader = new BufferedReader(new FileReader(csvFileName));
+			// ** Now using the OpenCSV **//
+			CSVReader reader = new CSVReader(csvReader, delimiter.charAt(0));
+			// CSVReader reader = new CSVReader(csvReader);
+			String[] nextLine;
+			int line = 0;
+			List<String> headers = new ArrayList<String>(5);
+			while ((nextLine = reader.readNext()) != null) {
 
-            //** Now using the OpenCSV **//
-            CSVReader reader = new CSVReader(csvReader, delimiter.charAt(0));
-            //CSVReader reader = new CSVReader(csvReader);
-            String[] nextLine;
-            int line = 0;
-            List<String> headers = new ArrayList<String>(5);
-            while ((nextLine = reader.readNext()) != null) {
+				if (line == 0) { // Header row
+					for (String col : nextLine) {
+						headers.add(col);
+					}
+				} else { // Data row
+					Element rowElement = newDoc.createElement("product");
+					rootElement.appendChild(rowElement);
 
-                if (line == 0) { // Header row
-                    for (String col : nextLine) {
-                        headers.add(col);
-                    }
-                } else { // Data row
-                    Element rowElement = newDoc.createElement("product");
-                    rootElement.appendChild(rowElement);
+					int col = 0;
+					for (String value : nextLine) {
+						String header = headers.get(col);
 
-                    int col = 0;
-                    for (String value : nextLine) {
-                        String header = headers.get(col);
+						Element curElement = newDoc.createElement(header);
+						curElement.appendChild(newDoc.createTextNode(value
+								.trim()));
+						rowElement.appendChild(curElement);
 
-                        Element curElement = newDoc.createElement(header);
-                        curElement.appendChild(newDoc.createTextNode(value.trim()));
-                        rowElement.appendChild(curElement);
+						col++;
+					}
+				}
+				line++;
+			}
+			// ** End of CSV parsing**//
 
-                        col++;
-                    }
-                }
-                line++;
-            }
-            //** End of CSV parsing**//
+			FileWriter writer = null;
 
-            FileWriter writer = null;
+			try {
 
-            try {
+				writer = new FileWriter(new File(xmlFileName));
 
-                writer = new FileWriter(new File(xmlFileName));
+				TransformerFactory tranFactory = TransformerFactory
+						.newInstance();
+				Transformer aTransformer = tranFactory.newTransformer();
+				aTransformer.setOutputProperty(OutputKeys.INDENT, "yes");
+				aTransformer.setOutputProperty(OutputKeys.METHOD, "xml");
+				aTransformer.setOutputProperty(
+						"{http://xml.apache.org/xslt}indent-amount", "4");
 
-                TransformerFactory tranFactory = TransformerFactory.newInstance();
-                Transformer aTransformer = tranFactory.newTransformer();
-                aTransformer.setOutputProperty(OutputKeys.INDENT, "yes");
-                aTransformer.setOutputProperty(OutputKeys.METHOD, "xml");
-                aTransformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+				Source src = new DOMSource(newDoc);
+				Result result = new StreamResult(writer);
+				aTransformer.transform(src, result);
 
-                Source src = new DOMSource(newDoc);
-                Result result = new StreamResult(writer);
-                aTransformer.transform(src, result);
+				writer.flush();
 
-                writer.flush();
+			} catch (Exception exp) {
+				exp.printStackTrace();
+			} finally {
+				try {
+					writer.close();
+				} catch (Exception e) {
+				}
+			}
 
-            } catch (Exception exp) {
-                exp.printStackTrace();
-            } finally {
-                try {
-                    writer.close();
-                } catch (Exception e) {
-                }
-            }
+			// Output to console for testing
+			// Result result = new StreamResult(System.out);
 
-            // Output to console for testing
-            // Result result = new StreamResult(System.out);
+		} catch (IOException exp) {
+			System.err.println(exp.toString());
+		} catch (Exception exp) {
+			System.err.println(exp.toString());
+		}
 
-        } catch (IOException exp) {
-            System.err.println(exp.toString());
-        } catch (Exception exp) {
-            System.err.println(exp.toString());
-        }
-        
-        // "XLM Document has been created" + rowsCount;
-    }
+		// "XLM Document has been created" + rowsCount;
+	}
 }
